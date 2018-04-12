06087a258b0163c2c2f19f3afff3847b58f6aae0
Edit <edit.axha@hotmail.com>
2018-04-12 16:06:27 +0000

    new CRUD project

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+class Article < ActiveRecord::Base
+  #validates :title, presence: true, length: {minimum: 3, maximum: 50}
+  #validate :description, presence: true, length: {minimum: 10, maximum: 300} #ben te detyrueshme pranine e nje vlere ne kete kolone, dhe numrin e karaktereve qe duhet te kete 
+  
+end
