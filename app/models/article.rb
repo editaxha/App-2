b7aeaa72f068544b1f1569533b6d92b15f244f1b
Edit <edit.axha@hotmail.com>
2018-04-13 15:25:17 +0000

    Fix create, add flash and error messages, create show

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-  #validates :title, presence: true, length: {minimum: 3, maximum: 50}
-  #validate :description, presence: true, length: {minimum: 10, maximum: 300} #ben te detyrueshme pranine e nje vlere ne kete kolone, dhe numrin e karaktereve qe duhet te kete 
+  validates :title, presence: true, length: {minimum: 3, maximum: 50}
+  validates :description, presence: true, length: {minimum: 10, maximum: 300} #ben te detyrueshme pranine e nje vlere ne kete kolone, dhe numrin e karaktereve qe duhet te kete 
   
 end
