6a07c3ee86fc2ccfaabd0d97e200cef15bcd56cd
Edit <edit.axha@hotmail.com>
2018-04-17 21:29:57 +0000

    prova

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '4.2.5'
 gem 'bcrypt', '~> 3.1.7'
 gem 'will_paginate', '3.0.7'
 gem 'bootstrap-will_paginate', '0.0.10'
+
+gem 'pg'
 # Use SCSS for stylesheets
 gem 'bootstrap-sass', '~> 3.3.7'
 gem 'sass-rails', '~> 5.0'
@@ -50,8 +52,7 @@ group :development do
 end
 
 group :production do
-   gem 'pg'
-
-  gem 'rails_12factor'
+   
+   gem 'rails_12factor'
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -16,9 +17,11 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  #sjdsjhdsd
   def edit
     @user = User.find(params[:id])
   end
+  
   def update
     @user =User.find(params[:id])
     if @user.update(user_params)
@@ -29,11 +32,11 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
   def show
     @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
-  
   
   private
   def user_params
