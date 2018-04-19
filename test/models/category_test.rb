e23cc69e31d509bc74cee59f77bae1a22c6f1739
Edit <edit.axha@hotmail.com>
2018-04-19 13:40:57 +0000

    Add categories model and tests

---
+++ test/models/category_test.rb
@@ -0,0 +1,43 @@
+require 'test_helper'
+
+class CategoryTest < ActiveSupport::TestCase
+  
+  def setup
+    @category = Category.new(name: "sports")
+    
+  end
+  
+  test "category should be valid" do #eshte e vlefshme kategoria apo jo
+    assert @category.valid?
+    
+  end
+  
+  test "name should be present" do #duhet te kete nje vlere, dhe ketu mund te jete dhe bosh
+    @category.name = " "
+    assert_not @category.valid?
+  end
+  
+  test "name should be unique" do #emri i kategorise te jete unike
+    @category.save
+    category2 = Category.new(name: "sports")
+    assert_not category2.valid?
+  end
+  
+  test "name should not be too long" do
+    @category.name = "a" * 26
+    assert_not @category.valid?
+  end
+  
+  test "name should not be too short" do
+    @category.name = "aa"
+    assert_not @category.valid?
+  end
+  
+end
+
+
+
+
+
+
+
